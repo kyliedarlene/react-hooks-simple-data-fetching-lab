4f70f2736c01a06ff145a3cd9f47777a04a92c0d
Kylie Clark <kyliedarlene@icloud.com>
2024-01-18 21:35:05 -0700

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,16 @@
 // create your App component here
+import React, {useState, useEffect} from "react";
+
+function App() {
+    const [image, setImage] = useState(false)
+    
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+            .then((resp) => resp.json())
+            .then((data) => setImage(data.message))
+    }, [])
+
+    return (image ? <img src={image} alt="A Random Dog"/> : <p>Loading...</p>)
+}
+
+export default App;
